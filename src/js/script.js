33d9e76096c01952e1af6f9f0124eec5bf774ba6
febryardiansyah <febrymuhammad80@gmail.com>
2020-10-02 20:38:21 +0700

    add scroll animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,28 @@
 $('.carousel').carousel({
     interval: 3000,
 })
+
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+$(".nav-item a").on('click', function(event) {
+
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+
+  }
+});
+
+$("#see-more").on("click",function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#our-projects").offset().top
+    }, 800);
+})
